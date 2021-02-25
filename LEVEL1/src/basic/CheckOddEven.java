459823b00ad3da8741e1702e1b1824dbced446dd
HP <HP@LAPTOP-KI77TKS7>
2021-02-25 15:25:14 +0530

    first

---
+++ LEVEL1/src/basic/CheckOddEven.java
@@ -0,0 +1,23 @@
+package basic;
+
+public class CheckOddEven {
+
+	public static void main(String[] args) {
+		
+		int n=8;
+		if(n%2!=0)
+		{
+			System.out.println("Odd Number");
+			
+		}
+		else
+		{
+			System.out.println("Even Number");
+		}
+		
+		System.out.println("BYE!");
+		System.exit(0);
+
+	}
+
+}
